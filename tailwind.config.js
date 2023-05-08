17a941e5a46e453105b97cc4948fb4ec5deb2744
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2023-05-08 16:47:33 -0700

    initial layout with html & tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/*.html", "./src/css/*.css", "./src/js/*.js"],
   theme: {
+    screens: {
+      // 'sm': '475px',
+      // => @media (min-width: 475px) { ... }
+      //min-width 475px: "if device width is 475 or larger, do this {}"
+      //max-width 475px: "if device width is 475 or smaller, do this {}"
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1028px',
+      // => @media (min-width: 1028px) { ... }
+    },
     extend: {},
   },
   plugins: [
