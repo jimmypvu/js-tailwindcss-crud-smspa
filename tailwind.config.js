c2573985f42528419c6ee0cc4dbddcc75bb7cb6c
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2023-05-09 18:24:44 -0700

    removed uuid dependency, updated js script tag in index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html", "./src/css/*.css", "./src/js/*.js"],
+  content: ["./*.html", "./src/css/*.css", "./src/js/*.js"],
   theme: {
     screens: {
       // 'sm': '475px',
@@ -15,10 +15,10 @@ module.exports = {
       // => @media (min-width: 1028px) { ... }
     },
     //don't need to change tailwind config if importing fonts into css
-    // fontFamily:{
-    //   display: ['Darumadrop One', 'cursive'],
-    //   display: ['Inter', 'sans-serif']
-    // },
+    fontFamily:{
+      display: ['Darumadrop One', 'cursive'],
+      display: ['Inter', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [
